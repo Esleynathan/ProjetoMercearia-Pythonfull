44ed26582a5bd53b8c141712798fa99cc5774891
Esley <esleynathan@hotmail.com>
2022-12-17 17:47:01 -0300

    Controller Venda #4 | Mostrar Vendas

--- controller.py
+++ controller.py
@@ -203,6 +203,33 @@ class ControllerVenda:
                 f"Quantidade: {i['quantidade']}\n")
             a += 1
 
+    def mostrarVenda(self,dataInicio, dataTermino):
+        vendas = DaoVenda.ler()
+        dataInicio1 = datetime.strptime(dataInicio,'%d/%m/%Y')
+        dataTermino1 = datetime.strptime(dataTermino,'%d/%m/%Y')
+        
+        vendasSelecionadas = list(filter(lambda x: datetime.strptime(x.data,'%d/%m/%Y') >= dataInicio1
+                                and datetime.strptime(x.data,'%d/%m/%Y') <= dataTermino1, vendas))
+        
+        cont = 1
+        total = 0
+        for i in vendasSelecionadas:
+            print(f'========== Venda [{cont}]==========')
+            print(f'Nome: {i.itensVendido.nome}\n'
+                f'Categoria: {i.itensVendido.categoria}\n'
+                f'Data: {i.data}\n'
+                f'Quantidade: {i.quantidadeVendida}\n'
+                f'Cliente: {i.comprador}\n'
+                f'Vendedor: {i.vendedor}\n')
+
+            total += int(i.itensVendido.preco) * int(i.quantidadeVendida)
+            cont += 1
+        print (f'Total vendido: {total}')
+
+
+
+
 a = ControllerVenda()
 #a.cadastrarVenda('abacaxi','erika','adailza',1)
-a.relatorioVendas()
+#a.relatorioVendas()
+a.mostrarVenda("07/12/2022", "20/12/2022")
