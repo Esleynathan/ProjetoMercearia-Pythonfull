d6e5de57b1f3e2c6830f6a639a88e03037f40213
Esley <esleynathan@hotmail.com>
2022-12-16 23:40:06 -0300

    Controller Estoque - Mostrar Estoque

--- controller.py
+++ controller.py
@@ -123,8 +123,22 @@ class ControllerEstoque:
         else:
             print("A categoria informada não existe")
 
+    def mostrarEstoque(self):
+        estoque = DaoEstoque.ler()
+        if len(estoque) == 0:
+            print ('Estoque vazio')
+        else:            
+            print("========== Produtos ==========")
+            for i in estoque:
+                print(f"Nome: {i.produto.nome}\n"
+                      f"Preço: {i.produto.preco}\n"
+                      f"Categoria: {i.produto.categoria}\n"
+                      f"Quantidade: {i.quantidade}\n")
+                print("----------")
+
 a = ControllerEstoque()
-a.alterarProduto('banana','maca','10','Verduras','10')        
+a.mostrarEstoque()
+
 
 class ControllerPessoa:
     pass
